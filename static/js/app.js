78571364a79a478115458e32c7de5e425f13af2f
Nik Walker <ketchupkid17@gmail.com>
2023-01-26 17:27:15 -0500

    created filtered table on datetime with filter button

--- static/js/app.js
+++ static/js/app.js
@@ -7,8 +7,6 @@ var tbody = d3.select("tbody");
 function buildTable(data) {
    // first clear out existing data
     tbody.html("");
-
-
 //loop through each object in the data 
 // and append a row and cells for each value in the row 
 //add forEach function
@@ -25,5 +23,31 @@ function buildTable(data) {
         );
     });
 }
+
+//add filters, button click on datetime
+function handleClick() {
+    //grab the datetime value from the filter
+    let date = d3.select("#datetime").property("value");
+    let filteredData = tableData;
+    
+    // filter the data to only show date entered
+    //check to see if a date was entered and filter the data using that date.
+    if (date) {
+        //apply 'filter' to the table data to only keep the rows
+        // where the 'datetime' value matches the filter value
+        filteredData= filteredData.filter(row => row.datetime === date);
+    };
+    //rebuild the table using the filtered data
+    // note: if no date was entered, then filteredData will
+    // just be the original tableData
+    buildTable(filteredData);
+// attach an event to listen for the form button (listen for click on the site)
+d3.selectAll("#filter-btn").on("click", handleClick);
+
+//build tabel whe th page loads (outside if statement table)
+buildTable(tableData);
+
+};
+
 
 
